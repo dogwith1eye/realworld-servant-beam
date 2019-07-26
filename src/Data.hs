2842cd8ed5bb0f0c7132a8f6d074231b9147dc44
dogwith1eye <dogwith1eye@gmail.com>
2019-07-26 15:33:54 -0400

    Added beam

---
+++ src/Data.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE StandaloneDeriving    #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+
+module Data where
+
+import Import hiding (id)
+import Data.Text (Text)
+import Database.Beam
+import Database.Beam.Postgres
+
+data UserT f = User
+  { _id :: Columnar f Int
+  , _password :: Columnar f Text
+  , _email :: Columnar f Text
+  , _username :: Columnar f Text
+  , _bio :: Columnar f Text
+  , _image :: Columnar f (Maybe Text)
+  } deriving (Generic, Beamable)
+
+type User = UserT Identity
+
+deriving instance Show User
+deriving instance Eq User
+deriving instance Ord User
+
+type UserId = PrimaryKey UserT Identity
+
+deriving instance Show UserId
+deriving instance Eq UserId
+deriving instance Ord UserId
+
+instance Table UserT where
+  data PrimaryKey UserT f
+      = UserId (Columnar f Int) deriving (Generic, Beamable)
+  primaryKey = UserId . _id
+
+data ConduitDb f = ConduitDb
+  { _conduitUsers :: f (TableEntity UserT)
+  } deriving (Generic)
+
+instance Database Postgres ConduitDb
+
+conduitDb :: DatabaseSettings Postgres ConduitDb
+conduitDb = defaultDbSettings
