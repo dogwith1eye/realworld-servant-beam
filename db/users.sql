38f9e4f7a912fa930dc32770c2384a2cf7f0db32
dogwith1eye <dogwith1eye@gmail.com>
2019-07-27 10:26:43 -0400

    Added postgres database

---
+++ db/users.sql
@@ -0,0 +1,12 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  password TEXT NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  username TEXT UNIQUE NOT NULL,
+  bio TEXT NOT NULL,
+  image TEXT
+);
+
+COMMIT;
