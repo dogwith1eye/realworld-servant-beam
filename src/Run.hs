c31ef67993f32e726b3ea4c0a38583e41a3fd53f
dogwith1eye <dogwith1eye@gmail.com>
2019-08-23 16:56:49 -0400

    Runs the query on every call to the endpoint

--- src/Run.hs
+++ src/Run.hs
@@ -17,19 +17,12 @@ type API = "users" :> Get '[JSON] [User]
 
 startApp ::  (HasLogFunc env, HasConnection env, HasPort env) => RIO env ()
 startApp = do
-  app <- conduit
   env <- ask
   logInfo $ "app is listening on port " <> (display (portL env))
-  liftIO $ run (portL env) app
-
-conduit :: (HasConnection env, HasLogFunc env) => RIO env Application
-conduit = serve api <$> server
+  liftIO $ run (portL env) $ serve api $ hoistServer api (runRIO env) server
 
 api :: Proxy API
 api = Proxy
 
-server :: (HasConnection env, HasLogFunc env) => RIO env (Server API)
-server = users
-
-users :: (HasConnection env, HasLogFunc env) => RIO env (Servant.Handler [User])
-users = pure <$> selectUsersDebug
+server :: (HasConnection env, HasLogFunc env) => ServerT API (RIO env)
+server = selectUsersDebug
